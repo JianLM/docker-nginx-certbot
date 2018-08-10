b19d6c95b53d224e62d598f4350831fc599181b7
David Lechner <david.lechner@talsen.team>
2018-08-10 14:17:23 +0200

    nginx logic update (start-up, shutdown)

--- docker/server-nginx-certbot/rootfs/opt/server-nginx-certbot/entrypoint.sh
+++ docker/server-nginx-certbot/rootfs/opt/server-nginx-certbot/entrypoint.sh
@@ -2,4 +2,19 @@
 
 set -euo pipefail
 
-echo "Hello, world!"
+echo -e "Starting nginx ..."
+
+/usr/sbin/nginx -g "daemon off;" & \
+VAR_PID_NGINX=${!}
+
+echo -e "Starting nginx ... \033[0;32mdone\033[0m"
+
+trap 'set -euo pipefail;                                                                         \
+      /bin/bash /opt/server-nginx-certbot/re-echo.sh "";                                         \
+      /bin/bash /opt/server-nginx-certbot/re-echo.sh "Stopping nginx ...";                                            \
+      kill -TERM ${VAR_PID_NGINX};                                                               \
+      /bin/bash /opt/server-nginx-certbot/re-echo.sh "Stopping nginx ... \033[0;32mdone\033[0m"; \
+      exit 0;'                                                                                   \
+      SIGTERM
+
+wait ${VAR_PID_NGINX}
