4c3a4c1b13a75fa9fb0820a9068f9b731d0ae08f
David Lechner <david.lechner@talsen.team>
2018-08-13 09:34:04 +0200

    bash docker command update (update-configuration)

--- bash-docker/command-update-configuration.sh
+++ bash-docker/command-update-configuration.sh
@@ -6,4 +6,4 @@ source ./util-functions.sh
 
 PrepareDockerComposeEnvironment ${@}
 
-docker-compose --file ${ENV_PATH_FILE_DOCKER_COMPOSE} exec server-nginx-certbot /bin/bash /opt/server-nginx-certbot/command-update-configuration.sh
+docker-compose --file ${ENV_PATH_FILE_DOCKER_COMPOSE} exec server-nginx-certbot /bin/bash /opt/server-nginx-certbot/command-update-configuration.sh ${2}
--- docker/server-nginx-certbot/rootfs/opt/server-nginx-certbot/command-update-configuration.sh
+++ docker/server-nginx-certbot/rootfs/opt/server-nginx-certbot/command-update-configuration.sh
@@ -2,4 +2,26 @@
 
 set -euo pipefail
 
-echo -e "Hello, world!"
+function ExportEnvironmentFile() {
+  local VAR_PATH_FILE_ENV="${1}"
+  if [ -f "${VAR_PATH_FILE_ENV}" ]; then
+    while IFS='' read -r VAR_LINE || [[ -n "${VAR_LINE}" ]]; do
+      if [ "${VAR_LINE}" = "" ]; then
+        continue;
+      fi
+      if [[ ${VAR_LINE} != \#* ]]; then
+        export "${VAR_LINE}"
+      fi
+    done < "${VAR_PATH_FILE_ENV}"
+  else
+    echo "Environment file '${VAR_PATH_FILE_ENV}' not found."
+    exit 1
+  fi
+}
+
+ExportEnvironmentFile "/etc/nginx/env/${1}.env"
+
+for VAR_VHOST_NAME in ${ENV_SERVICES_SERVER_NGINX_CERTBOT_VHOSTS}
+do
+  echo "${VAR_VHOST_NAME}"
+done
