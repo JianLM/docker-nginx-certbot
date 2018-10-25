aa8632124314bfc2a685c4d05a1c0b4e2e6270ad
David Lechner <david.lechner@talsen.team>
2018-10-25 14:12:03 +0200

    remove bash-docker

--- bash-docker/command-update-configuration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-source ./util-functions.sh
-
-PrepareDockerComposeEnvironment ${@}
-
-docker exec -t server-nginx-certbot /bin/bash /opt/server-nginx-certbot/command-update-configuration.sh ${2}
